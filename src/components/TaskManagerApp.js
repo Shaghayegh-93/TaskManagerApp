33155a29dc67082a356870ac501474f86fbcb9de
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-29 11:57:01 +0330

    make component as main component to render all of component in in and finally render this component to App component to have clean App component

--- src/components/TaskManagerApp.js
+++ src/components/TaskManagerApp.js
@@ -1,7 +1,61 @@
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+import { useState, useEffect } from "react";
+import { useTaskListAction } from "../context/TaskListProvider";
+
 const TaskManagerApp = () => {
-    return ( <div>
-        app
-    </div> );
-}
- 
+  const [edit, setEdit] = useState({ id: null, text: "" });
+
+  const [taskText, setTaskText] = useState("");
+
+  const dispatch = useTaskListAction();
+  useEffect(() => {
+    edit.id ? setTaskText(edit.text) : setTaskText("");
+  }, [edit]);
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setTaskText(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (edit.id) {
+      dispatch({
+        type: "EDIT_TASK",
+        payload: {
+          id: edit.id,
+          text: taskText,
+        },
+      });
+      setEdit({ id: null, text: "" });
+    }
+    if (!edit.id) {
+      const newTaskText = taskText.trim();
+      const isNewTaskTextPresent = newTaskText.length > 0;
+      if (isNewTaskTextPresent) {
+        dispatch({ type: "ADD_TASK", payload: newTaskText });
+        setTaskText("");
+      }
+    }
+  };
+
+  return (
+    <div className="app-wrapper">
+      <div className="main">
+        <header className="header">
+          <h1>Task Manager</h1>
+        </header>
+        <TaskForm
+          changeHandler={changeHandler}
+          taskText={taskText}
+          submitHandler={submitHandler}
+          edit={edit}
+        />
+        <TaskList setTaskText={setTaskText} setEdit={setEdit} />
+      </div>
+    </div>
+  );
+};
+
 export default TaskManagerApp;
