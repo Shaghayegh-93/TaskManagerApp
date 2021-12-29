c060fcddf4a5afab547d02853692161d8ab1868a
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-29 12:00:10 +0330

    add all reducer fnction to this taskListReducer

--- src/context/TaskListReducer.js
+++ src/context/TaskListReducer.js
@@ -1,8 +1,35 @@
 const taskListReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_TASK": {
+      const newTask = {
+        id: Math.ceil(Math.random() * 100),
+        text: action.payload,
+      };
+
+      return { ...state, taskList: [...state.taskList, newTask] };
+    }
+    case "REMOVE_ALL_TASK": {
+      return { ...state, taskList: "" };
+    }
+    case "REMOVE_ONE_TASK": {
+      const updatedTaskList = state.taskList.filter(
+        (task) => task.id !== action.payload
+      );
+      return { ...state, taskList: updatedTaskList };
+    }
+
+    case "EDIT_TASK": {
+      const updatedTaskList = state.taskList.map((task) => {
+        if (task.id === action.payload.id) {
+          return { ...task, text: action.payload.text };
+        }
+        return task;
+      });
+      return { ...state, taskList: updatedTaskList };
+    }
+
+    default:
+      return state;
   }
-
-
-  return state;
 };
 export default taskListReducer;
